6ef5244c79802614e866498b8ef2267a85d8b881
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-06 10:26:13 +0200

    finished this course module

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,28 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://cdn.pixabay.com/photo/2019/03/25/20/20/schnitzel-4081269_1280.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   /**
    * Gets a new array which is a copy of the current array
@@ -25,5 +32,9 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
