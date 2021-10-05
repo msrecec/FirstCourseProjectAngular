7223e71a6583c8cfe1dd876d2934b4d14790d459
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-05 09:12:00 +0200

    added params handing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,12 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
   collapsed: boolean = true;
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 
   constructor() {}
 
