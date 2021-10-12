13dbf0488841aa6b4a42ddd65d458491b6e911fb
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-12 17:57:58 +0200

    added storedata

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http
+      .put(
+        "https://ng-course-recipe-book-4ba43-default-rtdb.europe-west1.firebasedatabase.app/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+}
