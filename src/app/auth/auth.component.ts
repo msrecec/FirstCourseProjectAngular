4ad971c89456f8103fe7995e28090e8717f59e08
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-13 12:56:34 +0200

    added switch between auth modes and form input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
 export class AuthComponent implements OnInit {
+  isLoginMode: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
