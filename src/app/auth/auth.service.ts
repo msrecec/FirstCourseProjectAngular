658012134026450b410a63ef3feb497865641c0d
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-13 17:29:55 +0200

    added logout functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { BehaviorSubject, Subject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
@@ -21,7 +22,7 @@ export class AuthService {
   user = new BehaviorSubject<User>(null);
   token: string = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
     return this.http
@@ -59,6 +60,11 @@ export class AuthService {
           );
         })
       );
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(["/auth"]);
   }
 
   private handleAuthentication(
